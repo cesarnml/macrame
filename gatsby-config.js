3007ac96c18baaba72a696bbc26609eb83bfbefa
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-08-06 00:47:52 -0700

    adds gatsby-plugin-html-attributes

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en',
+      },
+    },
+    {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
         siteUrl: config.siteUrl,
