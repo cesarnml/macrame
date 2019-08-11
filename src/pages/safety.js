c1c5343fa21a7f3b507debdc5bd04c67b62974cb
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-08-10 19:50:59 -0500

    change nav and add safety page

--- src/pages/safety.js
+++ src/pages/safety.js
@@ -4,8 +4,13 @@ import config from 'utils/siteConfig'
 import Layout from 'components/Layout'
 import Container from 'components/Container'
 import PageTitle from 'components/PageTitle'
+import styled from "styled-components";
 
 import PropTypes from 'prop-types'
+
+const Concern = styled.h1`
+  font-size: 30px;
+`;
 
 const Safety = props => {
   return (
@@ -16,6 +21,23 @@ const Safety = props => {
 
       <Container>
         <PageTitle>Safety</PageTitle>
+         <Concern>Please use anonymous names and places.</Concern>
+            <p>
+              This is up to the writers discression. However, if you believe that information provided here could get someone else hurt or jeopardize their safety please consider either posting it anonymously or not posting it at all.
+            </p>
+            <Concern>This is not a resource for reporting crimes or seeking help.</Concern>
+            <p>
+              We do not have the resources or the ability to follow up with users and make sure they are safe. If your experiencing a crisis please contact the proper authorities where available.
+            </p>
+         
+              <li><a href="https://help.unhcr.org/" alt="link to UNHCR"></a>The UN Refugee Agency</li>
+              <li>International Committee of the Red Cross</li>
+            
+            <Concern>Stories have the right to be rejected by the editors on any grounds.</Concern>
+            <p>
+              While we endevour to share as many stories as possible we cannot publish all stories. Stories that contain strong language or hate speech will not be published.
+            </p>
+       
       </Container>
     </Layout>
   )
--- src/styles/global.js
+++ src/styles/global.js
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
    License: none (public domain)
   */
   html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  , h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
   del, dfn, em, img, ins, kbd, q, s, samp,
   small, strike, strong, sub, sup, tt, var,
@@ -90,7 +90,7 @@ const GlobalStyle = createGlobalStyle`
     text-rendering: optimizeLegibility;
     text-shadow: rgba(0, 0, 0, .01) 0 0 1px;
     font-weight: 400;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-family: 'Karla', sans-serif;
   }
 
   img {
